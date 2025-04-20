aef2f94a3333eb795792be702b69cf9af927a6e4
santhoshamuda <126236439+santhoshamuda@users.noreply.github.com>
2025-04-21 04:41:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# apt-get update && apt-get install -y cmake g++ python3-dev 
-# pip install --upgrade pip
-# pip install -r requirements.txt
+# apt-get update && apt-get install -y cmake g++ python3-dev #!/bin/bash
+apt-get update && apt-get install -y cmake g++ python3-dev python3-distutils
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
