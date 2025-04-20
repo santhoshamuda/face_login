c8d15a584e802878938a09a4cb66a56c9a84e048
santhoshamuda <126236439+santhoshamuda@users.noreply.github.com>
2025-04-21 04:05:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Update and install system packages required to build dlib
 apt-get update && apt-get install -y cmake g++ python3-dev
+pip install distutils
 
 # Install Python packages
 pip install -r requirements.txt
